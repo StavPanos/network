e34616d55f2c8df01856051695ecffa600729e4c
Tasos <Barous>
2019-04-02 15:43:25 +0300

    new

--- resources/views/components/social.blade.php
+++ resources/views/components/social.blade.php
@@ -1,11 +1,17 @@
-<div class="form-group row">
-    <div class="col-md-6 offset-md-4">
-        <a href="{{ url('/auth/github') }}" class="btn btn-github"><i class="fa fa-github"></i> Github</a>
+<div class="col-md-4">
+    <div class="form-group row">
+        <div class="col-md-6 offset-md-4">
+            <a href="{{ url('/auth/github') }}" class="btn btn-dark" style="width: 100%;">
+                <i class="fa fa-github"></i> Github
+            </a>
+        </div>
     </div>
-</div>
 
-<div class="form-group row">
-    <div class="col-md-6 offset-md-4">
-         <a href="{{ url('/auth/bitbucket') }}" class="btn btn-bitbucket"><i class="fa fa-bitbucket"></i> Bitbucket</a>
+    <div class="form-group row">
+        <div class="col-md-6 offset-md-4">
+             <a href="{{ url('/auth/bitbucket') }}" class="btn btn-primary" style="width: 100%;">
+                <i class="fa fa-bitbucket"></i> Bitbucket
+            </a>
+        </div>
     </div>
 </div>
