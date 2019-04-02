8563d5ab269782647149f80e309310f0a32861aa
Tasos <Barous>
2019-04-02 14:56:16 +0300

    new

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
     {{--FOR HTTPS HEROKU--}}
     <script src="https://dev-network-assignment.herokuapp.com/js/app.js"></script>
