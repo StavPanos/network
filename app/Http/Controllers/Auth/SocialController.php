0a5a9bb61f3703faadd27148bfa45f28f62a81b4
tasos <tasosbarous@gmail.com>
2019-04-26 14:16:00 +0300

    progress

--- app/Http/Controllers/Auth/SocialController.php
+++ app/Http/Controllers/Auth/SocialController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Carbon;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 use App\Models\User;
