7833110e8f4ae49a03b545c7315a63219085f227
tasos <tasosbarous@gmail.com>
2019-05-04 14:51:28 +0300

    progress

--- resources/views/repositories/index.blade.php
+++ resources/views/repositories/index.blade.php
@@ -7,15 +7,16 @@
 
 {{--    {{$repositories}}--}}
 
-    @foreach($repositories as $repo)
+
         <div class="card-body">
 
             <hr>
+            @foreach($repositories as $repo)
 
             <h3>{{$repo->name}}</h3>
             <p>{{$repo->description}}</p>
             <p>{{$repo->updated_at}}</p>
-
+@endforeach
         </div>
-    @endforeach
+
 </div>
