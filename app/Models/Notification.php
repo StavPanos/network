364dce53d08ca4904d19f5ee7dce29ee24f71f17
tasos <tasosbarous@gmail.com>
2019-05-04 16:44:26 +0300

    progress

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
-    protected $fillable = ['title'];
+    protected $fillable = ['title', 'type'];
 
     public function user()
     {
