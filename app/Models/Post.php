8fb295e27aa66e76a12570b72e49268c14f0e21d
tasos <tasosbarous@gmail.com>
2019-05-04 17:17:43 +0300

    progress

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,7 +12,7 @@ class Post extends Model
 
     public function user()
     {
-        return $this->belongsTo('App\Models\User');
+        return $this->belongsTo('App\Models\User')->orderBy('created_at', 'ASC');
     }
 
     public function replies()
