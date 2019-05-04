8cce15d15e80604f76c654ad7000405a4637ae6c
tasos <tasosbarous@gmail.com>
2019-05-04 14:18:28 +0300

    progress

--- app/Http/Controllers/Auth/SocialController.php
+++ app/Http/Controllers/Auth/SocialController.php
@@ -30,7 +30,7 @@ class SocialController extends Controller
     {
         $authUser = User::where('provider_id', $user->id)->first();
 
-        dd($user->repos_url);
+        dd($user);
 
         if ($authUser) {
             return $authUser;
