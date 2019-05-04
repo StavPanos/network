da7846fe644750e78d7713c1661faddecfc68236
tasos <tasosbarous@gmail.com>
2019-05-04 15:10:50 +0300

    progress

--- app/Http/Controllers/FriendController.php
+++ app/Http/Controllers/FriendController.php
@@ -55,7 +55,7 @@ class FriendController extends Controller
 
     public function friends()
     {
-        $friends = auth()->user()->friends;
+        $friends = auth()->user()->getFriends();
 
         return view('profile.friends', compact('friends'));
     }
