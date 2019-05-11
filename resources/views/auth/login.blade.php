b9b1459fd68366f63746ca2e094a9c086f286ec1
tasos <tasosbarous@gmail.com>
2019-05-11 15:40:39 +0300

    progress

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container mt-3">
-    <div class="row justify-content-center">
+    <div class="row">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Login</div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container mt-3">
-        <div class="row justify-content-center">
+        <div class="row">
             <div class="col-md-8">
                 @include('auth.register-member')
             </div>
