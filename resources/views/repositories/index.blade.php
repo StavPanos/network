39ca8a27db0f1cdc9c500623d2ba90d67b95c83e
tasos <tasosbarous@gmail.com>
2019-05-11 15:53:24 +0300

    progress

--- resources/views/repositories/index.blade.php
+++ resources/views/repositories/index.blade.php
@@ -14,7 +14,7 @@
                 <div class="card-body">
                     <p>{{$repo->description}}</p>
                     <p>
-                        last update at {{$repo->updated_at}}</p>
+                        last update at {{(\Carbon\Carbon::parse($repo->updated_at)->format('d/m/Y'))->diffForHumans() }}</p>
                 </div>
             </div>
             <br>
