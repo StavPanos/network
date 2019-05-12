92d96e63a59c90088bf026702e78aa42a3560733
tasos <tasosbarous@gmail.com>
2019-05-12 12:31:55 +0300

    progress

--- resources/views/friends/friendship.blade.php
+++ resources/views/friends/friendship.blade.php
@@ -3,7 +3,7 @@
         <div class="card">
             <div class="card-header">
                 @if(!auth()->user()->isFriendWith($user))
-                    <form method="post" action="/friend/connect">
+                    <form method="post" action="/friend/connect" class="d-inline-block">
                         {{csrf_field()}}
                         <input type="hidden" name="id" value="{{$user->id}}">
                         <button class="btn btn-primary btn-outline-primary d-inline-block" type="submit">
@@ -13,16 +13,16 @@
                     </form>
                 @else
                     <div class="text-muted">
-                        <form method="post" action="/friend/disconnect">
+                        <form method="post" action="/friend/disconnect" class="d-inline-block">
                             {{csrf_field()}}
                             <input type="hidden" name="id" value="{{$user->id}}">
-                            <button class="btn btn-danger btn-outline-danger d-inline-block" type="submit">Disconnect
+                            <button class="btn btn-danger btn-outline-danger " type="submit">Disconnect
                             </button>
                         </form>
                     </div>
                 @endif
 
-                <form method="post" action="friend/block">
+                <form method="post" action="friend/block" class="d-inline-block">
                     {{csrf_field()}}
                     <input type="hidden" name="id" value="{{$user->id}}">
                     <button class="btn btn-danger btn-outline-danger d-inline-block" type="submit">
