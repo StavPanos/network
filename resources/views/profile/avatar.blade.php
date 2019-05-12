6e40a06789c7b7d5a3df807abbdaf4fdd6c4a4b9
tasos <tasosbarous@gmail.com>
2019-05-12 12:18:02 +0300

    progress

--- resources/views/profile/avatar.blade.php
+++ resources/views/profile/avatar.blade.php
@@ -1,8 +1,8 @@
 @if($user->avatar)
     @if($user->custom_avatar)
-        <img src="/storage/avatars/{{$user->avatar}}" class="w-25">
+        <img src="/storage/avatars/{{$user->avatar}}">
     @else
-        <img src="{{$user->avatar}}" class="w-25">
+        <img src="{{$user->avatar}}">
     @endif
 @else
     <img src="{{asset('images/no-image.png')}}" alt="" class="w-25">
