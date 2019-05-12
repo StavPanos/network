05a55c878a1eb1f6768c896fa64078e613d2ccae
tasos <tasosbarous@gmail.com>
2019-05-12 12:25:39 +0300

    progress

--- resources/views/friends/friendship.blade.php
+++ resources/views/friends/friendship.blade.php
@@ -1,38 +1,41 @@
-@if(auth()->check())
-    <div class="col-lg-2">
-        <div class="card">
-            <div class="card-header">
+<div class="row">
+    @if(auth()->check())
+        <div class="col-lg-2">
+            <div class="card">
+                <div class="card-header">
 
-                @if(!auth()->user()->isFriendWith($user))
-                    <form method="post" action="/friend/connect">
-                        {{csrf_field()}}
-                        <input type="hidden" name="id" value="{{$user->id}}">
-                        <button class="btn btn-primary btn-outline-primary btn-block" type="submit">
-                            <i class="fa fa-plus"></i>
-                            Connect
-                        </button>
-                    </form>
-                @else
-                    <div class="text-muted">
-                        <form method="post" action="/friend/disconnect">
+                    @if(!auth()->user()->isFriendWith($user))
+                        <form method="post" action="/friend/connect">
                             {{csrf_field()}}
                             <input type="hidden" name="id" value="{{$user->id}}">
-                            <button class="btn btn-danger btn-outline-danger btn-block" type="submit">Disconnect</button>
+                            <button class="btn btn-primary btn-outline-primary btn-block" type="submit">
+                                <i class="fa fa-plus"></i>
+                                Connect
+                            </button>
                         </form>
-                    </div>
-                @endif
+                    @else
+                        <div class="text-muted">
+                            <form method="post" action="/friend/disconnect">
+                                {{csrf_field()}}
+                                <input type="hidden" name="id" value="{{$user->id}}">
+                                <button class="btn btn-danger btn-outline-danger btn-block" type="submit">Disconnect
+                                </button>
+                            </form>
+                        </div>
+                    @endif
 
-                <br>
+                    <br>
 
-                <form method="post" action="friend/block">
-                    {{csrf_field()}}
-                    <input type="hidden" name="id" value="{{$user->id}}">
-                    <button class="btn btn-danger w-100" type="submit">
-                        <i class="fa fa-ban"></i>
-                        Block
-                    </button>
-                </form>
+                    <form method="post" action="friend/block">
+                        {{csrf_field()}}
+                        <input type="hidden" name="id" value="{{$user->id}}">
+                        <button class="btn btn-danger w-100" type="submit">
+                            <i class="fa fa-ban"></i>
+                            Block
+                        </button>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-@endif
+    @endif
+</div>
