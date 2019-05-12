d9542c0275cd61d915056a640afe8cf944b9d1df
tasos <tasosbarous@gmail.com>
2019-05-12 12:24:51 +0300

    progress

--- resources/views/profile/view.blade.php
+++ resources/views/profile/view.blade.php
@@ -3,13 +3,13 @@
 @section('content')
     <div class="container mt-3">
         <div class="row justify-content-center">
+            @include('friends.friendship', ['user'=>$user])
             @include('profile.profile', ['user' => $user])
             <div class="col-md-6">
                 @include('repositories.index', ['user' => $user])
                 @include('projects.projects', ['user'=>$user])
                 @include('posts.posts', ['user'=>$user])
             </div>
-            @include('friends.friendship', ['user'=>$user])
         </div>
     </div>
 @endsection
